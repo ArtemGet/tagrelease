ed5d30aadc6207f5ea8e742edbf20c66356acd9e
Artem Getmanskii <re910@yandex.ru>
2025-05-13 13:43:32 +0300

    add tag exception

---
+++ src/main/java/io/github/artemget/tagrelease/exception/TagException.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget.tagrelease.exception;
+
+import java.io.Serial;
+
+/**
+ * Throws at build/get tag.
+ *
+ * @since 0.1.0
+ */
+public class TagException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 4172661814037122452L;
+
+    public TagException(String message) {
+        super(message);
+    }
+
+    public TagException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TagException(Throwable cause) {
+        super(cause);
+    }
+}
